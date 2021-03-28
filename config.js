c949b4a8c944c8362d892ea214b270a9fb4beed2
Judy Bui <36145425+JudyBui127@users.noreply.github.com>
2021-03-28 00:31:37 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+let HOST = process.env.HOST || "localhost";
+let PORT = process.env.PORT || 81;
+module.exports={
+    MONGO_URI:`mongodb+srv://judy127:judy127asdf@cluster0.cfgze.mongodb.net/STOCKMARKET?retryWrites=true&w=majority`,
+    secretString: 'secretjudy12345',
+    HOST,
+    PORT}
+    
+ 
